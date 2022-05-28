ef931c814b6fc921d46742e62baec5c9e928cff2
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:11:56 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs17.js
@@ -0,0 +1,16 @@
+function tugas17a() {
+    var kata1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk"
+    var kata2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual"
+    console.log(kata1.toLowerCase());
+    console.log(kata1.toUpperCase());
+    console.log(kata2.toLowerCase());
+    console.log(kata2.toUpperCase());
+}
+
+function tugas17b(){
+    var kata = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    console.log(kata.charCodeAt(0)+" - "+kata.charCodeAt(1)+" - "+kata.charCodeAt(2)+" - "+kata.charCodeAt(3)+" - "+kata.charCodeAt(4)+" - "+kata.charCodeAt(5));
+}
+
+tugas17a();
+tugas17b();
